ebb0d100cb9608fe92675c29d035084a4aab0d02
phfs2 <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>
2025-06-24 23:31:57 -0300

    feat: recebimento de mensagens no servidor

--- primeira_entrega/server.py
+++ primeira_entrega/server.py
@@ -1,5 +1,6 @@
 import socket
 import queue
+import threading
 from functions import *
 
 # Tamanaho do Buffer
@@ -14,6 +15,40 @@ clientes = {}
 
 # Fila que armazenará as mensagens que devem ser enviadas
 filaMsg = queue.Queue()
+
+# Função utilizada para receber as mensagens do clienteAdd commentMore actions
+def receberMsg():
+    fullMensage = ''  # Irá armazenar a mensagem completa
+
+    
+    while True:
+        mensagem, clientAddr = socketServer.recvfrom(BUFFER_SIZE)
+
+        mensagem = mensagem.decode('ISO-8859-1')  # Decodificando o fragmento de mensagem
+ 
+        if mensagem == "<EOF>":  # Quando for uma flag dizendo que o arquivo foi trasmitido
+            
+            caminho = converterToTxt(clientes[clientAddr], fullMensage, True) # Convertendo a mensagem em um arquivo TXT
+
+            # Abrindo o arquivo TXT
+            with open(caminho) as file:
+                contentFile = file.read()
+            
+            # Inserido na fila o conteudo do arquivo TXT
+            filaMsg.put((contentFile, clientAddr))
+            fullMensage = ''  # Esvaziando a variável para a proxima mensagem
+        
+        # Se tiver sido enviando uma flag para entrada/saída do usuário da sala
+        elif mensagem.startswith("LOGIN:") or mensagem.startswith("LOGOUT:"):
+            filaMsg.put((mensagem, clientAddr))
+
+        # Adicionando o fragmento de mensagem na váriavel que armazará a mensagem completa
+        else:
+            fullMensage += mensagem
+
+# Criando uma Thread para receber as mensagens
+threadReceber = threading.Thread(target=receberMsg)
+threadReceber.start()
 
 # Função para enviar para todos que estiverem no chat
 def sendToAll(mensagem:str, clientes:dict):
