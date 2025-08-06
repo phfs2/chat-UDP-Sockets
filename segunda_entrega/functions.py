388d9e6835c6fd2eb933e275de978ea31575eaef
Ithalo Araujo <araujoithalo@gmail.com>
2025-08-06 17:58:12 -0300

    add criar pacote

--- segunda_entrega/functions.py
+++ segunda_entrega/functions.py
@@ -1,24 +1,27 @@
-import crcmod;
+import crcmod
+import struct
 
 def converterToTxt(username, texto, isServer = False):
 
-    if not isServer:
-        caminho = f'./segunda_entrega/dados/client/{username}.txt'
+    def calcularChecksum(data:bytes):
+     return checksum
+
+
+def criarPacote(msg:bytes, seq:int, ack:int):
+
+    # Payload da mensagem a ser enviada
+    payload = msg
+
+    # Header sem o Checksum 
+    pseudoHeader = struct.pack('!BB', seq, ack)
+
+    # Calculando o checksum com as informações do cabeçalho e o payload
+    checksum = calcularChecksum(pseudoHeader+payload)
+
+    # Header de 4 Bytes (Seq - 1 Byte, Ack - 1 Byte, Checksum - 2 Bytes)
+    header = struct.pack('!BBH', seq, ack, checksum)
     
-    else:
-        caminho  = f'./segunda_entrega/dados/server/{username}.txt'
+    # Criando o pacote (Cabeçalho + Dados)
+    pacote = header+payload
 
-    with open(caminho, "w") as file:
-        file.write(texto)
-
-    return caminho
-
-def calcularChecksum(data:bytes):
-
-    # Obtendo a função que que calcula o checksum CRC-16
-    crc16 = crcmod.predefined.mkCrcFun("crc-16")
-    
-    # Calculando o Checksum
-    checksum = crc16(data)
-
-    return checksum
+    return pacote  # Retornando o pacote já pronto para envio
